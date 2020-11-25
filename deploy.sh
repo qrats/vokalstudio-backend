a40116c1a701c5ddf745b8311fe3547275f8abd6
pycoach <updev675@gmail.com>
2020-11-25 16:38:56 -0500

    nginx script updated

--- deploy.sh
+++ deploy.sh
@@ -18,10 +18,12 @@ systemctl daemon-reload
 service virtualstudio restart
 
 echo "Nginx reload..."
-cp -f /var/www/virtualstudio/backend/scripts/virtualstudio.conf /etc/nginx/sites-available/
+sudo apt-get install nginx -y
+cp -f /var/www/virtualstudio/backend/scripts/virtualstudio.host /etc/nginx/sites-available/
 ln -s -f /etc/nginx/sites-available/virtualstudio.conf /etc/nginx/sites-enabled/
 service nginx restart
 
 echo "Celery reload..."
+sudo apt-get install supervisor -y
 cp -f /var/www/virtualstudio/backend/scripts/celery-worker.conf /etc/supervisor/conf.d/
 supervisorctl reload
