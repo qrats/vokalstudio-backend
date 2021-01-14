74a5ecc81ead2e31c46b23939d4291fa7a92ce86
pycoach <updev675@gmail.com>
2021-01-14 11:47:21 -0500

    minor fix

--- src/resources/media_configuration.py
+++ src/resources/media_configuration.py
@@ -206,7 +206,7 @@ class GetMediaURLsResource(Resource):
             media_urls = []
             for user in users:
                 item = {
-                    'url': f"{app.config['HOST_NAME']}/api/media/configuration/{user.user_id}",
+                    'url': f"https://api.virtualstudio.host/api/media/configuration/{user.user_id}",
                     'name': user.name
                 }
                 media_urls.append(item)
